98c82dac56ef96187ce5dd880058bcfc60e1fc6a
vuonga1103 <vuonga1103@yahoo.com>
2020-07-13 14:03:32 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,25 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+        # renders super hero form
+        erb :super_hero
+    end
 
+    post '/team' do
+        # create a team out of info user submitted
+        @team = Team.new(params[:team])
+
+        # loop through the array hashes of heroes user submitted
+        params[:team][:heros].each do |hero_details|
+            #create a new hero out of the hero details provided
+            Hero.new(hero_details)
+        end
+
+        # assign to instance var heros the array of the heros created
+        @heros = Hero.all
+
+        # renders created team page
+        erb :team
+    end
 end
---
+++ app/models/hero.rb
@@ -0,0 +1,20 @@
+class Hero
+  attr_accessor :name, :power, :bio
+  @@all = []
+
+  def initialize(args)
+    @name = args[:name]
+    @power = args[:power]
+    @bio = args[:bio]
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all.clear
+  end
+end
